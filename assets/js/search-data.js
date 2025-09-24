c2a4038a37998e26ef0e9fe3da82aeadcf5916dc
Hmirault <51160214+Hmirault@users.noreply.github.com>
2025-09-24 21:08:51 +0000

    Deploying to gh-pages from @ Hmirault/Hmirault.github.io@7ee48f1968c262a7a421678a4783819c0a97d86e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%68%6D%69%72%61%75%6C%74@%61%75%67%75%73%74%61.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -522,7 +522,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=WZtCsUsAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
